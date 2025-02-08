cade2784dbdf7ee3b7e44cca465ce7a2b5fc4823
Michal Vala <michal.vala@gmail.com>
2025-02-08 15:20:04 +0100

    cleanup

--- telemetry.go
+++ telemetry.go
@@ -15,6 +15,20 @@ type accTelemetry struct {
 	graphicsData *accDataHolder[AccGraphic]
 }
 
+type accDataHolder[T AccGraphic | AccPhysics | AccStatic] struct {
+	mmap *mmap
+	data *T
+}
+
+func (d *accDataHolder[T]) Close() error {
+	if d.mmap != nil {
+		d.mmap.Close()
+		d.mmap = nil
+	}
+	d.data = nil
+	return nil
+}
+
 func (t *accTelemetry) Connect() error {
 	var accStatic AccStatic
 	staticMMap, err := mapFile(STATIC_FILE_MMAP, unsafe.Sizeof(accStatic))
